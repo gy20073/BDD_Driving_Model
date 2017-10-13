fc9047b37dae6c24170868db2de0ee4bdc2559f8
Yang Gao <gy20073@gmail.com>
2017-10-13 10:02:18 -0700

    add perspective augmentation

--- data_providers/nexar_large_speed.py
+++ data_providers/nexar_large_speed.py
@@ -8,7 +8,6 @@ import numpy as np
 import os, random
 import util
 import util_car
-import math
 import scipy.misc as misc
 import glob
 import multiprocessing
@@ -16,6 +15,9 @@ from cStringIO import StringIO
 from PIL import Image
 import cv2
 import ctypes
+import math, copy
+from scipy import interpolate
+
 FLAGS = tf.app.flags.FLAGS
 
 
@@ -96,6 +98,11 @@ tf.app.flags.DEFINE_boolean('is_MKZ_dataset', False,
                             'The newly collected data on the Lincoln MKZ')
 tf.app.flags.DEFINE_boolean('use_non_random_shuffle', False,
                             'Whether to shuffle the files')
+
+tf.app.flags.DEFINE_integer('crop_car_hood', -1,
+                            'crop the bottom x pixels, if >0. Pixels counted after resize. ')
+tf.app.flags.DEFINE_boolean('use_perspective_augmentation', False,
+                            'Whether to perspective augmentation')
 
 
 # the newly designed class has to have those methods
@@ -746,6 +753,9 @@ class MyDataset(Dataset):
             #decoded = tf.image.resize_nearest_neighbor(decoded, new_size)
             decoded = tf.cast(decoded, tf.uint8)
 
+        if FLAGS.crop_car_hood > 0:
+            decoded = decoded[:, :-FLAGS.crop_car_hood, :, :]
+
         speed = features['image/speeds'].values
         speed = tf.reshape(speed, [-1, 2])
         speed = speed[:FLAGS.FRAMES_IN_SEG, :]
@@ -913,4 +923,108 @@ class MyDataset(Dataset):
                 images = tf.cast(images, tf.uint8)
             net_inputs[0] = images
 
+        if FLAGS.use_perspective_augmentation:
+            images = net_inputs[0] # shape:: N * F * HWC
+            images_shape = [x.value for x in images.get_shape()]
+            future_labels = net_outputs[2]  # shape: N * F * 2
+            future_labels_shape = [x.value for x in future_labels.get_shape()]
+
+            images, future_labels = tf.py_func(MyDataset.perspective_changes,
+                                               [images, future_labels],
+                                               [tf.uint8, tf.float32])
+
+            images = tf.reshape(images, [images_shape[0]*images_shape[1], images_shape[2], images_shape[3], images_shape[4]])
+            images = tf.image.resize_bilinear(images, (228, 228))
+            images = tf.cast(images, tf.uint8)
+            images = tf.reshape(images, [images_shape[0], images_shape[1], 228, 228, images_shape[4]])
+
+            future_labels.set_shape(future_labels_shape)
+            net_inputs[0] = images
+            net_outputs[2] = future_labels
+
         return net_inputs, net_outputs
+
+    @staticmethod
+    def generate_meshlist(arange1, arange2):
+        return np.dstack(np.meshgrid(arange1, arange2, indexing='ij')).reshape((-1, 2))
+
+
+    # the input should be bottom cropped image, i.e. no car hood
+    @staticmethod
+    def rotate_ground(original, theta, horizon=60, half_height=360 / 2, focal=1.0):
+        height, width, channel = original.shape
+        # the target grids
+        yp = range(height - horizon, height)
+        xp = range(0, width)
+
+        # from pixel to coordinates
+        y0 = (np.array(yp) - half_height) * 1.0 / half_height
+        x0 = (np.array(xp) - width / 2) / (width / 2.0)
+
+        # form the mesh
+        mesh = MyDataset.generate_meshlist(x0, y0)
+        # compute the source coordinates
+        st = math.sin(theta)
+        ct = math.cos(theta)
+        deno = ct * focal + st * mesh[:, 0]
+        out = np.array([(-st * focal + ct * mesh[:, 0]) / deno, mesh[:, 1] / deno])
+
+        # interpolate
+        vout = []
+        for i in range(3):
+            f = interpolate.RectBivariateSpline(y0, x0, original[- horizon:, :, i])
+            values = f(out[1, :], out[0, :], grid=False)
+            vout.append(values)
+
+        lower = np.reshape(vout, (3, width, horizon)).transpose((2, 1, 0)).astype("uint8")
+
+        # compute the upper part
+        out = np.reshape(out[0, :], (width, horizon))
+        out = out[:, 0]
+        f = interpolate.interp1d(x0, original[:-horizon, :, :], axis=1,
+                                 fill_value=(original[:-horizon, 0, :], original[:-horizon, -1, :]),
+                                 bounds_error=False)
+        ans = f(out)
+        ans = ans.astype("uint8")
+
+        return np.concatenate((ans, lower), axis=0)
+
+
+    @staticmethod
+    def perspective_changes(images, future_labels):
+        N, F, H, W, C = images.shape
+        N2, F2, C2 = future_labels.shape
+        assert (N == N2)
+        assert (F == F2)
+        assert (C2 == 2)
+
+        perspective_aug_prob = 0.1
+        perspective_recover_time = 2.0 # second
+        perspective_theta_std = 0.15
+        # image related
+        horizon = 60
+        half_height = 360 / 2
+        focal = 1.0
+
+        # precomputed constants
+        downsampled_framerate = FLAGS.frame_rate / FLAGS.temporal_downsample_factor
+        num_frames = int(perspective_recover_time * downsampled_framerate)
+
+        for ni in range(N):
+            i = 0
+            while i+num_frames < F:
+                if random.random() < perspective_aug_prob :
+                    # then we need to augment the images starting from this one
+                    # random sample a rotate angle
+                    theta = random.gauss(0, perspective_theta_std)
+                    yaw_rate_delta = -theta / perspective_recover_time
+
+                    for j in range(num_frames):
+                        # distort each of the frames and yaw rate
+                        images[ni, i, :, :, :] = MyDataset.rotate_ground(
+                                    images[ni, i, :, :, :], theta*(1-1.0*j/num_frames), horizon, half_height, focal)
+                        future_labels[ni, i, 0] += yaw_rate_delta
+                        i += 1
+                else:
+                    i += 1
+        return [images, future_labels]
